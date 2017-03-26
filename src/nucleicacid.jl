ba23a9ad2b4a9d9e01b0556e5ae633d319352c65
Kenta Sato (佐藤 建太) <bicycle1885@gmail.com>
2017-03-26 19:50:43 +0900

    restructure files in type-centric way (#4)

--- src/nucleicacid.jl
+++ src/nucleicacid.jl
@@ -1,7 +1,7 @@
-# typedefs.jl
-# ===========
+# Nucleic Acid
+# ============
 #
-# Definitions of NucleicAcid and AminoAcid types.
+# DNA and RNA types.
 #
 # This file is a part of the BioJulia ecosystem.
 # License is MIT: https://github.com/BioJulia/NucleicAcids.jl/blob/master/LICENSE.md
@@ -70,9 +70,45 @@ function Base.convert(::Type{Char}, nt::RNA)
     return rna_to_char[convert(UInt8, nt) + 1]
 end
 
+# Print
+# -----
+
+function Base.show(io::IO, nt::DNA)
+    if isvalid(nt)
+        if nt == DNA_Gap
+            write(io, "DNA_Gap")
+        else
+            write(io, "DNA_", Char(nt))
+        end
+    else
+        write(io, "Invalid DNA")
+    end
+    return
+end
+
+function Base.show(io::IO, nt::RNA)
+    if isvalid(nt)
+        if nt == RNA_Gap
+            write(io, "RNA_Gap")
+        else
+            write(io, "RNA_", Char(nt))
+        end
+    else
+        write(io, "Invalid RNA")
+    end
+    return
+end
+
+function Base.print(io::IO, nt::NucleicAcid)
+    if !isvalid(nt)
+        throw(ArgumentError("nucleic acid is invalid"))
+    end
+    write(io, Char(nt))
+    return
+end
+
 # Encoding of DNA and RNA NucleicAcids
 # ------------------------------------
-# DNA
 
 # lookup table for characters
 const char_to_dna = [0x80 for _ in 0x00:0xff]
@@ -106,13 +142,12 @@ for (char, doc, bits) in [
     end
 end
 
-@eval alphabet(::Type{DNA}) = $(tuple([reinterpret(DNA, x)
-                                                 for x in 0b0000:0b1111]...))
+@eval function alphabet(::Type{DNA})
+    return $(tuple([reinterpret(DNA, x) for x in 0b0000:0b1111]...))
+end
 
 const ACGT = (DNA_A, DNA_C, DNA_G, DNA_T)
 const ACGTN = (DNA_A, DNA_C, DNA_G, DNA_T, DNA_N)
-
-# RNA
 
 # lookup table for characters
 const char_to_rna = [0x80 for _ in 0x00:0xff]
@@ -143,137 +178,146 @@ for (char, doc, dna) in [
     end
 end
 
-@eval alphabet(::Type{RNA}) = $(tuple([reinterpret(RNA, x)
-                                                 for x in 0b0000:0b1111]...))
+@eval function alphabet(::Type{RNA})
+    return $(tuple([reinterpret(RNA, x) for x in 0b0000:0b1111]...))
+end
 
 const ACGU = (RNA_A, RNA_C, RNA_G, RNA_U)
 const ACGUN = (RNA_A, RNA_C, RNA_G, RNA_U, RNA_N)
 
-
-# Aminoacids
-# ==========
-#
-# The amino acid type.
-
-"Type representing AminoAcids"
-@compat primitive type AminoAcid 8 end
-
-# Conversion from/to integers
-# ---------------------------
-
-Base.convert(::Type{AminoAcid}, aa::UInt8) = reinterpret(AminoAcid, aa)
-Base.convert(::Type{UInt8}, aa::AminoAcid) = reinterpret(UInt8, aa)
-Base.convert{T<:Number}(::Type{T}, aa::AminoAcid) = convert(T, UInt8(aa))
-Base.convert{T<:Number}(::Type{AminoAcid}, aa::T) = convert(AminoAcid, UInt8(aa))
-
-# Conversion from/to Char
-# -----------------------
-
-function Base.convert(::Type{AminoAcid}, c::Char)
-    @inbounds aa = c <= '\x7f' ? char_to_aa[Int(c)+1] : AA_INVALID
-    @assert aa != AA_INVALID error("$(c) is not a valid amino acid")
-    return aa
+function Base.:~{N<:NucleicAcid}(x::N)
+    return reinterpret(N, ~reinterpret(UInt8, x) & 0b1111)
 end
 
-Base.convert(::Type{Char}, aa::AminoAcid) = aa_to_char[convert(UInt8, aa) + 1]
-
-# Amino acid encoding definition
-# ------------------------------
-
-"Invalid Amino Acid"
-const AA_INVALID = convert(AminoAcid, 0x1c)  # Used during conversion from strings
-
-# lookup table for characters
-const char_to_aa = [AA_INVALID for _ in 0x00:0x7f]
-const aa_to_char = Vector{Char}(0x1c)
-
-# compatibility bits
-const compatbits_aa = Vector{UInt32}(28)
-
-# This set of amino acids is defined by IUPAC-IUB Joint Commission on Biochemical Nomenclature.
-# Reference: http://www.insdc.org/documents/feature_table.html#7.4.3
-
-for (aa, doc, code) in [
-        ('A', "Alanine",                           0x00),
-        ('R', "Arginine",                          0x01),
-        ('N', "Asparagine",                        0x02),
-        ('D', "Aspartic Acid",                     0x03),
-        ('C', "Cysteine",                          0x04),
-        ('Q', "Glutamine",                         0x05),
-        ('E', "Glutamic Acid",                     0x06),
-        ('G', "Glycine",                           0x07),
-        ('H', "Histidine",                         0x08),
-        ('I', "Isoleucine",                        0x09),
-        ('L', "Leucine",                           0x0a),
-        ('K', "Lysine",                            0x0b),
-        ('M', "Methionine",                        0x0c),
-        ('F', "Phenylalanine",                     0x0d),
-        ('P', "Proline",                           0x0e),
-        ('S', "Serine",                            0x0f),
-        ('T', "Threonine",                         0x10),
-        ('W', "Tryptophan",                        0x11),
-        ('Y', "Tyrosine",                          0x12),
-        ('V', "Valine",                            0x13),
-        ('O', "Pyrrolysine",                       0x14),  # non-standard
-        ('U', "Selenocysteine",                    0x15),  # non-standard
-        ('B', "Aspartic Acid or Asparagine",       0x16),  # ambiguous
-        ('J', "Leucine or Isoleucine",             0x17),  # ambiguous
-        ('Z', "Glutamine or Glutamic Acid",        0x18),  # ambiguous
-        ('X', "Unspecified or Unknown Amino Acid", 0x19)]  # ambiguous
-    var = Symbol("AA_", aa)
-    @eval begin
-        @doc $doc const $var = convert(AminoAcid, $code)
-        char_to_aa[$(Int(aa)+1)] = char_to_aa[$(Int(lowercase(aa))+1)] = $var
-        aa_to_char[$(code)+1] = $aa
-    end
-    if code ≤ 0x15
-        compatbits_aa[code+1] = 1 << code
-    elseif code == 0x16
-        compatbits_aa[code+1] = 1 << 0x02 | 1 << 0x03
-    elseif code == 0x17
-        compatbits_aa[code+1] = 1 << 0x09 | 1 << 0x0a
-    elseif code == 0x18
-        compatbits_aa[code+1] = 1 << 0x05 | 1 << 0x06
-    elseif code == 0x19
-        compatbits_aa[code+1] = (1 << 0x16) - 1
-    end
+function Base.:|{N<:NucleicAcid}(x::N, y::N)
+    return reinterpret(N, reinterpret(UInt8, x) | reinterpret(UInt8, y))
 end
 
-"Terminal"
-const AA_Term = convert(AminoAcid, 0x1a)
-char_to_aa[Int('*')+1] = AA_Term
-aa_to_char[0x1a+1] = '*'
-compatbits_aa[0x1a+1] = 1 << 0x1a
+function Base.:&{N<:NucleicAcid}(x::N, y::N)
+    return reinterpret(N, reinterpret(UInt8, x) & reinterpret(UInt8, y))
+end
 
-"Amino Acid Gap"
-const AA_Gap = convert(AminoAcid, 0x1b)
-char_to_aa[Int('-') + 1] = AA_Gap
-aa_to_char[0x1b+1] = '-'
-compatbits_aa[0x1b+1] = 0
+function Base.:-{N<:NucleicAcid}(x::N, y::N)
+    return Int(x) - Int(y)
+end
 
-@eval alphabet(::Type{AminoAcid}) = $(tuple([reinterpret(AminoAcid, x) for x in 0x00:0x1b]...))
+function Base.:-{N<:NucleicAcid}(x::N, y::Integer)
+    return x + (-y)
+end
 
-# lookup table of 20 standard amino acids
-const threeletter_to_aa = Dict(
-    "ALA" => AA_A, "ARG" => AA_R, "ASN" => AA_N, "ASP" => AA_D, "CYS" => AA_C,
-    "GLN" => AA_Q, "GLU" => AA_E, "GLY" => AA_G, "HIS" => AA_H, "ILE" => AA_I,
-    "LEU" => AA_L, "LYS" => AA_K, "MET" => AA_M, "PHE" => AA_F, "PRO" => AA_P,
-    "SER" => AA_S, "THR" => AA_T, "TRP" => AA_W, "TYR" => AA_Y, "VAL" => AA_V,
-    "ASX" => AA_B, "XLE" => AA_J, "GLX" => AA_Z, "XAA" => AA_X,
-    "PYL" => AA_O, "SEC" => AA_U,
-)
+function Base.:+{N<:NucleicAcid}(x::N, y::Integer)
+    return reinterpret(N, (UInt8(x) + y % UInt8) & 0b1111)
+end
 
-function Base.parse(::Type{AminoAcid}, s::AbstractString)
-    s′ = strip(s)
-    if length(s′) == 1
-        return convert(AminoAcid, s′[1])
-    end
-    try
-        return threeletter_to_aa[uppercase(s′)]
-    catch ex
-        if isa(ex, KeyError)
-            error("invalid amino acid string: \"$s\" ")
-        end
-        rethrow()
-    end
+function Base.isless{N<:NucleicAcid}(x::N, y::N)
+    return isless(reinterpret(UInt8, x), reinterpret(UInt8, y))
+end
+
+@inline function Base.count_ones(nt::NucleicAcid)
+    return count_ones(reinterpret(UInt8, nt))
+end
+
+function Base.trailing_zeros(nt::NucleicAcid)
+    return trailing_zeros(reinterpret(UInt8, nt))
+end
+
+function gap{N<:NucleicAcid}(::Type{N})
+    return reinterpret(N, 0b0000)
+end
+
+"""
+    isGC(nt::NucleicAcid)
+Test if `nt` is surely either guanine or cytosine.
+"""
+function isGC(nt::NucleicAcid)
+    bits = reinterpret(UInt8, nt)
+    return bits != 0 && (bits & 0b1001) == 0
+end
+
+"""
+    ispurine(nt::NucleicAcid)
+Test if nucleotide is surely a purine.
+"""
+@inline function ispurine(nt::NucleicAcid)
+    bits = reinterpret(UInt8, nt)
+    return bits != 0 && (bits & 0b1010) == 0
+end
+
+"""
+    ispyrimidine(nt::NucleicAcid)
+Test if nucleotide is surely a pyrimidine.
+"""
+@inline function ispyrimidine(nt::NucleicAcid)
+    bits = reinterpret(UInt8, nt)
+    return bits != 0 && (bits & 0b0101) == 0
+end
+
+"""
+    isambiguous(nt::NucleicAcid)
+Test if `nt` is ambiguous nucleotide.
+"""
+@inline function isambiguous(nt::NucleicAcid)
+    return count_ones(nt) > 1
+end
+
+"""
+    iscertain(nt::NucleicAcid)
+Test if `nt` is a non-ambiguous nucleotide e.g. ACGT.
+"""
+@inline function iscertain(nt::NucleicAcid)
+    return count_ones(nt) == 1
+end
+
+"""
+    isgap(nt::NucleicAcid)
+Test if `nt` is a gap.
+"""
+@inline function isgap(nt::NucleicAcid)
+    return count_ones(nt) == 0
+end
+
+"""
+    complement(nt::NucleicAcid)
+Return the complementary nucleotide of `nt`.
+"""
+function complement(nt::NucleicAcid)
+    bits = compatbits(nt)
+    return reinterpret(
+        typeof(nt),
+        (bits & 0x01) << 3 | (bits & 0x08) >> 3 |
+        (bits & 0x02) << 1 | (bits & 0x04) >> 1)
+end
+
+function Base.isvalid{T<:NucleicAcid}(::Type{T}, x::Integer)
+    return 0 ≤ x < 16
+end
+
+function Base.isvalid(nt::NucleicAcid)
+    return reinterpret(UInt8, nt) ≤ 0b1111
+end
+
+"""
+    iscompatible(x, y)
+Return `true` if and only if `x` and `y` are compatible with each other (i.e.
+`x` and `y` can be the same symbol).
+`x` and `y` must be the same type (`DNA`, `RNA` or `AminoAcid`).
+# Examples
+```julia
+julia> iscompatible(DNA_A, DNA_A)
+true
+julia> iscompatible(DNA_C, DNA_N)  # DNA_N can be DNA_C
+true
+julia> iscompatible(DNA_C, DNA_R)  # DNA_R (A or G) cannot be DNA_C
+false
+julia> iscompatible(AA_A, AA_X)    # AA_X can be AA_A
+true
+```
+"""
+@inline function iscompatible{T<:NucleicAcid}(x::T, y::T)
+    return compatbits(x) & compatbits(y) != 0
+end
+
+# Return the compatibility bits of `nt`.
+@inline function compatbits(nt::NucleicAcid)
+    return reinterpret(UInt8, nt)
 end
