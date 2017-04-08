f70c1ca7824173b69f073c0b3f2fc6e25f4803d0
Ben J. Ward <ward9250@gmail.com>
2017-04-08 14:17:43 +0100

    Fix to make.jl in docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, BiologicalSymbols
 makedocs()
 deploydocs(
     deps = Deps.pip("mkdocs", "pygments", "mkdocs-biojulia"),
-    repo = "github.com/BioJulia/BiologicalSymbols.jl.git",
+    repo = "github.com/BioJulia/BioSymbols.jl.git",
     julia = "0.5",
     osname = "linux",
 )
