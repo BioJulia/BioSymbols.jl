d7ca03b332d1c28562b7f87268a79ac64c11eb65
Kenta Sato <bicycle1885@gmail.com>
2017-07-06 16:44:34 +0900

    use material

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter, BioSymbols
 
 makedocs()
 deploydocs(
-    deps = Deps.pip("mkdocs", "pygments", "mkdocs-biojulia"),
+    deps = Deps.pip("mkdocs", "pygments", "mkdocs-material"),
     repo = "github.com/BioJulia/BioSymbols.jl.git",
     julia = "0.6",
     osname = "linux",
