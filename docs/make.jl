90f36256943e4650c452d4af6666816aa51feed4
Ben J. Ward <ward9250@gmail.com>
2018-08-11 17:54:55 +0100

    Update project files

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,22 @@
 using Documenter, BioSymbols
 
-makedocs()
+makedocs(
+    format = :html,
+    sitename = "BioSymbols",
+    pages = [
+        "Home" => "index.md",
+        "Nucleic Acids" => "nucleicacids.md",
+        "Amino Acids" => "aminoacids.md",
+        "Alphabets" => "alphabets.md",
+        "Sequences" => "sequences.md",
+        "References" => "references.md"
+    ],
+    authors = "Ben J. Ward, The BioJulia Organisation and other contributors."
+)
+
 deploydocs(
     deps = Deps.pip("mkdocs", "pygments", "mkdocs-material"),
     repo = "github.com/BioJulia/BioSymbols.jl.git",
-    julia = "0.6",
+    julia = "1.0",
     osname = "linux",
 )
