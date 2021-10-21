60d47c7c97d729c76ecd011e9b6cf80c834e8b3b
jamiecm17 <moffajamie@gmail.com>
2021-10-21 12:02:40 -0500

    Shortened BioSymbols docstring

--- src/BioSymbols.jl
+++ src/BioSymbols.jl
@@ -10,126 +10,13 @@ __precompile__()
 
 """
 A package to define types for amino acids and nucleic acids and to perform operations on these types.
-
-Amino Acid Symbols:
-- `AA_A`: Alanine 
-- `AA_B`: Aspartic Acid or Asparagine 
-- `AA_C`: Cysteine 
-- `AA_D`: Aspartic Acid 
-- `AA_E`: Glutamic Acid 
-- `AA_F`: Phenylalanine 
-- `AA_G`: Glycine 
-- `AA_Gap`: Amino Acid Gap 
-- `AA_H`: Histidine 
-- `AA_I`: Isoleucine 
-- `AA_J`: Leucine or Isoleucine 
-- `AA_K`: Lysine 
-- `AA_L`: Leucine 
-- `AA_M`: Methionine 
-- `AA_N`: Asparagine 
-- `AA_O`: Pyrrolysine 
-- `AA_P`: Proline 
-- `AA_Q`: Glutamine 
-- `AA_R`: Arginine 
-- `AA_S`: Serine 
-- `AA_T`: Threonine 
-- `AA_Term`: Terminal 
-- `AA_U`: Selenocysteine 
-- `AA_V`: Valine 
-- `AA_W`: Tryptophan 
-- `AA_X`: Unspecified or Unknown Amino Acid 
-- `AA_Y`: Tyrosine 
-- `AA_Z`: Glutamine or Glutamic Acid 
-
-- `ACGT`: Unambiguous DNA.
-- `ACGTN`: Unambiguous DNA and `DNA_N`.
-- `ACGU`: Unambiguous RNA.
-- `ACGUN`: Unambiguous RNA and `RNA_N`.
-- `AminoAcid`: An amino acid type.
-- `BioSymbol`: The BioSymbol type is an abstract type that represents any kind of biological symbol that may appear in data such as biological sequences, SNP datasets and more.
-
-DNA Symbols:
-- `DNA`: A deoxyribonucleic acid type.
-- `DNA_A`: DNA Adenine 
-- `DNA_B`: DNA Cytosine, Guanine or Thymine 
-- `DNA_C`: DNA Cytosine 
-- `DNA_D`: DNA Adenine, Guanine or Thymine 
-- `DNA_G`: DNA Guanine 
-- `DNA_Gap`: DNA Gap 
-- `DNA_H`: DNA Adenine, Cytosine or Thymine 
-- `DNA_K`: DNA Guanine or Thymine 
-- `DNA_M`: DNA Adenine or Cytosine 
-- `DNA_N`: DNA Adenine, Cytosine, Guanine or Thymine 
-- `DNA_R`: DNA Adenine or Guanine 
-- `DNA_S`: DNA Cytosine or Guanine 
-- `DNA_T`: DNA Thymine 
-- `DNA_V`: DNA Adenine, Cytosine or Guanine 
-- `DNA_W`: DNA Adenine or Thymine 
-- `DNA_Y`: DNA Cytosine or Thymine 
-- `NucleicAcid`: An abstract nucleic acid type.
-
-RNA Symbols:
-- `RNA`: A ribonucleic acid type.
-- `RNA_A`: RNA Adenine 
-- `RNA_B`: RNA Cytosine, Guanine or Uracil 
-- `RNA_C`: RNA Cytosine 
-- `RNA_D`: RNA Adenine, Guanine or Uracil 
-- `RNA_G`: RNA Guanine 
-- `RNA_Gap`: RNA Gap 
-- `RNA_H`: RNA Adenine, Cytosine or Uracil 
-- `RNA_K`: RNA Guanine or Uracil 
-- `RNA_M`: RNA Adenine or Cytosine 
-- `RNA_N`: RNA Adenine, Cytosine, Guanine or Uracil 
-- `RNA_R`: RNA Adenine or Guanine 
-- `RNA_S`: RNA Cytosine or Guanine 
-- `RNA_U`: RNA Uracil 
-- `RNA_V`: RNA Adenine, Cytosine or Guanine 
-- `RNA_W`: RNA Adenine or Uracil 
-- `RNA_Y`: RNA Cytosine or Uracil 
+For more information see:
+- Amino Acid Symbols: https://github.com/BioJulia/BioSymbols.jl/blob/master/docs/src/aminoacids.md
+- Nucleic Acid Symbols: https://github.com/BioJulia/BioSymbols.jl/blob/master/docs/src/nucleicacids.md
+- Creating Sequences: https://github.com/BioJulia/BioSymbols.jl/blob/master/docs/src/sequences.md
 
 Functions:
-- `alphabet`:
-  + Get all symbols of `AminoAcid` in sorted order.
-  + Get all symbols of `RNA` in sorted order.
-  + Get all symbols of `DNA` in sorted order.
-
-- `compatbits`:
-  + Return the compatibility bits of `aa` as `UInt32`.
-  + Return the compatibility bits of `nt` as `UInt8`.
-  - `complement`: Return the complementary nucleotide of `nt`.
-
-- `gap`:
-  + Return `AA_Gap`.
-  + Return `RNA_Gap`.
-  + Return `DNA_Gap`.
-
-- `isGC`: Test if `nt` is surely either guanine or cytosine.
-
-- `isambiguous`:
-  + Test if `aa` is an ambiguous amino acid.
-  + Test if `nt` is an ambiguous nucleotide.
-
-- `iscertain`:
-  + Test if `aa` is a non-ambiguous amino acid.
-  + Test if `nt` is a non-ambiguous nucleotide e.g.
-
-- `iscompatible`: Test if `x` and `y` are compatible with each other.
-
-- `isgap`: Test if `symbol` is a gap.
-
-- `ispurine`: Test if `nt` is surely a purine.
-
-- `ispyrimidine`: Test if `nt` is surely a pyrimidine.
-
-- `BioSymbols.AA_INVALID`: Invalid Amino Acid 
-
-- `BioSymbols.char_to_dna`: Lookup table used for converting characters to DNA symbol values The provided `convert` method should be used rather than this table, but you can use it if you insist and know what your are doing.
-
-- `BioSymbols.char_to_rna`: Lookup table used for converting characters to RNA symbol values The provided `convert` method should be used rather than this table, but you can use it if you insist and know what your are doing.
-
-- `BioSymbols.dna_to_char`: Lookup table for converting DNA symbol values to characters The provided `convert` method should be used rather than this table, but you can use it if you insist and know what your are doing.
-
-- `BioSymbols.rna_to_char`: Lookup table for converting RNA symbol values to characters The provided `convert` method should be used rather than this table, but you can use it if you insist and know what your are doing.
+- `alphabet`, `compatbits`, `gap`, `isGC`, `isambiguous`, `iscertain`, `iscompatible`, `isgap`, `ispurine`, `ispyrimidine`
 """
 module BioSymbols
 
